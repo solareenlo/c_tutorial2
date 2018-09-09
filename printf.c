6724be8e64b0e9ceaae4b274b82bb055955bdbb5
solareenlo <solareenlo@gmail.com>
2018-09-09 20:59:55 +0900

    printf関数の練習パート２

---
+++ printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main(void)
+{
+	printf("こんにちは。私の名前は%sです。\n年齢は%dです。\n", "山田太郎", 20);
+	printf("イニシャルは、%cです。\n", 'Y');
+	printf("%f + %f = %f\n", 1.2, 2.7, 1.2 + 2.7);
+
+	return 0;
+}
