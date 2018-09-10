ecf85140c285da5a7cc02ef435fafbe32724f870
solareenlo <solareenlo@gmail.com>
2018-09-10 10:25:15 +0900

    変数の練習

---
+++ variable.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+ * 変数を用いた計算
+ */
+
+int main(void)
+{
+	int a;
+	int b = 3;
+	int add, sub;
+	double avg;
+
+	a = 10;
+	add = a + b;
+	sub = a - b;
+	avg = (a + b) / 2.0;
+
+	printf("%d + %d = %d\n", a, b, add);
+	printf("%d - %d = %d\n", a, b, sub);
+	printf("%dと%dの平均値 : %f\n", a, b, avg);
+
+	return 0;
+}
