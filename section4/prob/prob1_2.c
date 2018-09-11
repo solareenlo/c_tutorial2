4cc8205539ec18e1c53342d895d96bf7455615b1
solareenlo <solareenlo@gmail.com>
2018-09-11 10:31:11 +0900

    演算の練習

---
+++ section4/prob/prob1_2.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("123\n456\n789\n");
+	return 0;
+}
---
+++ section4/prob/prob1_3.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main(void) {
+	printf("%d + %d = %d\n", 1, 1, 1+1);
+	printf("%d + %d = %d\n", 2, 3, 2+3);
+	return 0;
+}
