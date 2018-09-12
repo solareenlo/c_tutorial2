16c137804f783317678cc624c5a91a9f1e29e80f
solareenlo <solareenlo@gmail.com>
2018-09-12 10:19:09 +0900

    while文とdo~while文の違いを練習（回数に０を入力すると分かりやすい）

---
+++ section6/while_do_while.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(void) {
+	int i, num;
+
+	printf("回数を入力：");
+	scanf("%d", &num);
+
+	printf("whileで実行\n");
+	i = 1;
+	while (i <= num) {
+		printf("*");
+		i++;
+	}
+	printf("\n");
+
+	printf("do~whileで実行\n");
+	i = 1;
+	do {
+		printf("*");
+		i++;
+	} while (i <= num);
+	printf("\n");
+
+	return 0;
+}
