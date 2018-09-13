56b168a8fe349da24c701836271345754a961a53
solareenlo <solareenlo@gmail.com>
2018-09-14 05:52:08 +0900

    グローバル変数とファイル分割の練習

---
+++ intro7/list7_2/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "calc.h"
+#include "showResult.h"
+
+int main(void){
+    int a = 2, b =3;
+
+    printf("%d + %d = ", a, b);
+    add(a, b);
+    showAnswer();
+
+    printf("%d - %d = ", a, b);
+    sub(a, b);
+    showAnswer();
+
+    return 0;
+}
